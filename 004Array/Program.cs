03294312dffce220916564d54ae2b035f5e10dcd
Alyona <mirka-sobaka@inbox.ru>
2022-09-21 11:07:07 +0400

    задачка на одномерный массив с поиском элемента

--- 004Array/Program.cs
+++ 004Array/Program.cs
@@ -20,25 +20,45 @@ void PrintArray(int [] col)
         position++;
      }
 }
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // если искомого элемента нет, то будет -1 в результате
 
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 
 int[] array = new int [10]; // определили массив из 10 элементов
 
 FillArray(array); //метод заполнил массив
 PrintArray(array); // метод распечатал массив
+Console.WriteLine();
 
-int n = array.Length;
-int find = 8;
-int index = 0;
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
 
-while(index < n)
-{
-    if(array[index] == find)
-    {
-        Console.WriteLine(index);
-        break;
-    }
-    //index = index +1;
-       index++;
+// int n = array.Length;
+// int find = 8;
+// int index = 0;
 
-}
+// while(index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     //index = index +1;
+//        index++;
+
+// }
