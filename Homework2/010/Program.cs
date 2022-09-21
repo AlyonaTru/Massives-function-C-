631f8d84361c1e6b4ebe66279246f4bc4be89dad
Alyona <mirka-sobaka@inbox.ru>
2022-09-21 15:34:57 +0400

    задачка 010

--- Homework2/010/Program.cs
+++ Homework2/010/Program.cs
@@ -2,3 +2,18 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+void Zadacha010()
+{
+   Console.WriteLine("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int number1 = number / 100; // первое число
+    int number2 = number / 10 % 10; //искомое второе число
+    int number3 = number % 10; // третье число
+
+    Console.WriteLine(" " + number2);
+    
+   
+}
+Zadacha010();
