2410fa86c2401182f948eddc6d349b0318da3239
Alyona <mirka-sobaka@inbox.ru>
2022-09-22 12:01:51 +0400

    добавила задачу 013

--- Homework2/013/Program.cs
+++ Homework2/013/Program.cs
@@ -1,5 +1,32 @@
 ﻿
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+void Zadacha013() 
+{
+    Console.WriteLine("Введите число : ");
+    string number = Console.ReadLine();
+    char[] ch = number.ToCharArray();
+    int length = number.Length;
+    
+    if (ch[0] == '-') 
+    {
+            Console.WriteLine(ch[3]);
+    }
+    
+    else if (length < 3)
+        {
+           Console.WriteLine("Третьей цифры нет");
+        }
+    
+    
+    else 
+        {
+           Console.WriteLine( ch[2]);
+                  }
+}
+Zadacha013();
