ec7dca27095dddce0fd5c13a029b2daa56967d7c
Alyona <mirka-sobaka@inbox.ru>
2022-09-26 14:26:37 +0400

    задачка про  таблицу кубов

---
+++ Homework3/023/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Zadacha023()
+{
+
+Console.WriteLine("Введите число: ");
+int a  = Convert.ToInt32(Console.ReadLine());
+int i =1;
+double Sqw;
+
+while (i <= a)
+{
+    Sqw = Math.Pow(i, 3);
+    Console.WriteLine($"|{i}|{Sqw}|");
+    i++;
+}
+}
+Zadacha023();
