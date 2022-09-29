d3a00b0189767f4fac08c8d86bc3f47e9f0e99c6
Alyona <mirka-sobaka@inbox.ru>
2022-09-29 12:27:16 +0400

    задача 025 на возведение числа в степень = второму числу

--- Homework4/025/Program.cs
+++ Homework4/025/Program.cs
@@ -15,3 +15,21 @@ Console.WriteLine(result);
 
 }
 Zadacha025();
+
+// или другое решение если не брать класс Math
+// Console.Write("Введите число A: ");
+// double a = Double.Parse(Console.ReadLine() ?? "0");
+// Console.Write("Введите число B: ");
+// int b = Int32.Parse(Console.ReadLine() ?? "0");
+
+// Console.WriteLine(Power(a, b));
+
+// double Power(double x, int y)
+// {
+//     double pow = x;
+//     for(int i = 1; i < y; i++)
+//     {
+//         pow *= x;
+//     }
+//     return pow;
+// }
