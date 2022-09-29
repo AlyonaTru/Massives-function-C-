a8af6700b9665c78947f1a37f63f97a06dfb1323
Alyona <mirka-sobaka@inbox.ru>
2022-09-29 12:23:46 +0400

    задача 025 на возведение числа в степень = второму числу

---
+++ Homework4/029/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]           6, 1, 33 -> [6, 1, 33]
+
+void Zadacha029()
+{
+    Random random = new Random();
+    int size = random.Next(1, 100);
+    int [] numbers = new int [size];
+    Console.WriteLine($"размер массива = {size}");
+   
+  
+
+   
+// for (int i = 0; i < size; i++)
+// {
+//     Console.WriteLine(numbers[i] + "\t");
+// }
+
+// Console.WriteLine();
+
+
+}
+Zadacha029();
