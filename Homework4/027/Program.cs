41c859899f39ae22333a7b0ed142dd027d975965
Alyona <mirka-sobaka@inbox.ru>
2022-09-29 10:26:17 +0400

    задача 027 на сумму цифр в числе

---
+++ Homework4/027/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+//82 -> 10
+// 9012 -> 12
+
+void Zadacha027()
+{
+Console.WriteLine("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+for (int i = 0; number > 0; i++)
+{
+int digit = number%10;
+number = number /10;
+result = result + digit;
+
+}
+
+{
+    Console.WriteLine("Ошибка ввода.");
+}
+Console.WriteLine(result);
+}
+Zadacha027();
