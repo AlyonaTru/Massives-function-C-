7e20ef10edd4d1e8ceb4ca2d5f1530ac76354bd3
Alyona <mirka-sobaka@inbox.ru>
2022-10-03 13:57:14 +0400

    папка 033

---
+++ Zadachi/035/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив, состоящий из случ чисел. Найдите кол-во элементов массива, которые лежат в отрезке
+// [10, 99]
+
+void Zadacha035()
+{
+    int size = 5;
+    int[]numbers = new int[size];
+    
+    FillArray(numbers);
+    PrintArray(numbers); 
+    FindArray(numbers);
+}
+Zadacha035();
+
+void FillArray(int[]numbers)
+{
+    Random random = new Random ();
+    for (int i =0; i< numbers.Length; i++)
+    {
+        numbers[i] = random.Next(-10, 100);
+    }
+}
+
+void PrintArray(int[]numbers)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i =0; i<numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void FindArray(int[]numbers)
+{
+    int count = 0;
+  for (int i =0; i<numbers.Length; i++)
+    {
+        if (numbers[i] >= 10 && numbers[i] <= 99)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество принадлежащих заданному промежутку чисел = {count}");
+}
---
+++ Zadachi/037/Program.cs
@@ -0,0 +1,43 @@
+﻿// найдите пр-е пар чисел в одномерном массиве. Парой считаем 1 и посл, 2 и предпосл элементы и тд. 
+// Результат запишите в новом массиве.
+
+void Zadacha037()
+{
+    int size = 11;
+    int[]numbers = new int[size];
+    
+    FillArray(numbers);
+    PrintArray(numbers); 
+    
+int maxIndex = size-1;
+for(int i =0; i< size/2; i++)
+{
+    Console.WriteLine($"{numbers[i]}*{numbers[maxIndex - i]} = {numbers[i]*numbers[maxIndex - i]} ");
+}
+if (size % 2 ==1)
+{
+    Console.WriteLine($" средний элемент массива = {numbers[size/2]}"); // вывод среднего числа массива при нечетном значении размера
+}
+
+}
+Zadacha037();
+
+void FillArray(int[]numbers)
+{
+    Random random = new Random ();
+    for (int i =0; i< numbers.Length; i++)
+    {
+        numbers[i] = random.Next(-10, 100);
+    }
+}
+
+void PrintArray(int[]numbers)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i =0; i<numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
