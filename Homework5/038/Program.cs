a83746ce45bced32deaf7ed211ca485eb5005326
Alyona <mirka-sobaka@inbox.ru>
2022-10-03 14:24:24 +0400

    задача на разницу макс и мин элемента вещ массива

--- Homework5/038/Program.cs
+++ Homework5/038/Program.cs
@@ -1,2 +1,48 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
+
+void Zadacha038()
+{
+    int size = 5;
+    double[] numbers = new double[size];
+    
+    FillArray(numbers);
+    PrintArray(numbers); // вызов двух методов
+    DiffMinMax(numbers);
+}
+Zadacha038();
+
+void FillArray(double[]numbers)
+{
+    Random random = new Random ();
+    for (int i =0; i< numbers.Length; i++)
+    {
+        numbers[i] = random.NextDouble();
+        numbers[i] = Math.Round(numbers[i], 3);
+        
+    }
+}
+
+void PrintArray(double[]numbers)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i =0; i<numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void DiffMinMax(double[]numbers)
+{ 
+    double diff=0;
+    double min = numbers.Min();
+    double max = numbers.Max();
+    for (int i =0; i< numbers.Length; i++)
+      {
+         diff = numbers.Max() - numbers.Min();
+        }
+    
+    Console.WriteLine($"разница =  {diff}.");   
+       
+}
