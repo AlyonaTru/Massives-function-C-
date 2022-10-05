a26244466e750f86d9a6e960b8d6944aa7f0a6b7
Alyona <mirka-sobaka@inbox.ru>
2022-10-05 12:36:20 +0400

    задача на переод числа в двоичную систему счисления

---
+++ Zadachi/042/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101 3->11 2->10
+
+void Zadacha042()
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    double numAs2 = 0;
+    double i = 0;
+    while(number > 0)
+    {
+        numAs2 = (number%2) * Math.Pow(10, i) + numAs2;
+        number = number/2;
+        i++;
+    }
+    Console.WriteLine($"В двоичном коде это {numAs2} ");
+}
+Zadacha042();
