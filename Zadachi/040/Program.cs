bcaf8a6789fb3baafadabd8e87b8ef5ad95ecfde
Alyona <mirka-sobaka@inbox.ru>
2022-10-05 11:28:18 +0400

    задача на стороны треугольника

---
+++ Zadachi/040/Program.cs
@@ -0,0 +1,28 @@
+﻿// программа, которая принимает на вход 3 числа и проверяет, 
+//может ли существовать треугольник со сторонами такой длины
+
+void Zadacha040()
+{
+    Console.WriteLine("Введите число А");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число В");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число C");
+    int c = Convert.ToInt32(Console.ReadLine());
+
+    if (a<=0 || b<=0 ||c<=0)
+    {
+        Console.WriteLine("такого треугольника нет");
+    }
+    
+    else if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.WriteLine("такой треугольник существует");
+    }
+ 
+    }
+
+Zadacha040();
+
+
+
