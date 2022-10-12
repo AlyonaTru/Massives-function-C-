f4dc960d6a9e1c51f6e4ff59b2a138604830b26b
Alyona <mirka-sobaka@inbox.ru>
2022-10-12 17:57:28 +0400

    seminar 8

---
+++ Zadachi/053/Program.cs
@@ -0,0 +1,62 @@
+﻿// задайте двумерный массив. Напишите программу, которая поменяет первую и последнюю строки массива местами
+
+void Zadacha053()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    Console.WriteLine($"Массив размера {rows}x{columns}");
+    int[,]numbers = new int[rows,columns];
+    FillArray(numbers);
+    PrintArray(numbers);
+    Console.WriteLine("Перевернули массив");
+    ChangeElements(numbers);
+    PrintArray(numbers);
+
+    
+}
+Zadacha053();
+   
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random ();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+     for (int i = 0; i< rows; i++)
+    {
+        for (int j = 0; j< columns; j++)
+        {
+         numbers[i,j] = random.Next(-10, 11);
+        }
+        }
+}
+
+void PrintArray(int[,] numbers)
+ {
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i =0; i< rows; i++)
+    {
+        for (int j =0; j< columns; j++)
+        {
+            Console.Write(numbers[i,j] + " \t");
+        }
+        Console.WriteLine();
+    }
+ }
+
+void ChangeElements(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    int maxIndex = rows-1;
+    int maxColumns = numbers.GetLength(1);
+  
+    for (int j =0; j < maxColumns; j++)
+    {
+        (numbers[0,j], numbers[maxIndex,j]) = (numbers[maxIndex,j], numbers[0,j]);
+        
+    }
+    Console.WriteLine();
+}
---
+++ Zadachi/055/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задайте двумерный массив. Наиишите программу, которая заменяет строки на столбцы. В случае, если 
+// это невозможно, программа должна вывести сообщение пользователю.
+
+void Zadacha055()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8); // или сразу поставить заглушку int rows = columns (но тогда условие неточно)
+    int columns = random.Next(4, 8);
+    Console.WriteLine($"Массив размера {rows}x{columns}");
+    int[,]numbers = new int[rows,columns];
+    FillArray(numbers);
+    PrintArray(numbers);
+    if (rows == columns)
+    {
+    Console.WriteLine("Поменяли строки на столбцы:");
+    ChangeRowsForColumns(numbers);
+    PrintArray(numbers);
+    }
+    else 
+    Console.WriteLine("Задача невыполнима");
+       
+}
+Zadacha055();
+   
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random ();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+     for (int i = 0; i< rows; i++)
+    {
+        for (int j = 0; j< columns; j++)
+        {
+         numbers[i,j] = random.Next(-10, 11);
+        }
+        }
+}
+
+void PrintArray(int[,] numbers)
+ {
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i =0; i< rows; i++)
+    {
+        for (int j =0; j< columns; j++)
+        {
+            Console.Write(numbers[i,j] + " \t");
+        }
+        Console.WriteLine();
+    }
+ }
+
+void ChangeRowsForColumns(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int maxColumns = numbers.GetLength(1);
+      for (int i = 0; i< rows; i++)
+    {
+        for (int j =i; j < maxColumns; j++)
+        {
+        (numbers[i,j], numbers[j,i]) = (numbers[j,i], numbers[i,j]);
+        
+        }
+        Console.WriteLine();
+}
+   
+}
