dc4ba46aed63462fc1239e1011fe6857ba3641a4
Alyona <mirka-sobaka@inbox.ru>
2022-10-17 12:43:32 +0400

    proekt  9

---
+++ Homework9/068/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД)
+// этих чисел с помощью рекурсии.   M = 28; N = 7 -> 7
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(Divider(m, n)); 
+
+int Divider(int m, int n)
+{ 
+    if (m ==n) return m;
+    if (m < n) return Divider(m, n-m);
+    else return Divider(m -n, n);
+    
+ } 
+Divider(m, n);
